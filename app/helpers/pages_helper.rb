bc3d32e6fa446afb62c59d2838a28296c49971b4
Ivan Shelomentsev <Shelomentsev.Ivan@gmail.com>
2017-01-29 06:56:33 +0500

    Added markdown method to PagesHelper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,12 @@
 module PagesHelper
+  def markdown(text)
+    text.gsub!(/\*\*([^\*]*)\*\*/) { |s| s.delete!('*'); s="<b>"+s+"</b>" }
+    text.gsub!(/\\\\([^\\]*)\\\\/) { |s| s.delete!('\\'); s="<i>"+s+"</i>" }
+    text.gsub!(/\(\(([^\(\)]*)\)\)/) do |s|
+      s.delete!('()')
+      s=s.split(' ')
+      s="<a href=\"#{s[0]}\">"+s[1]+"</a>"
+    end
+    text.html_safe
+  end
 end
